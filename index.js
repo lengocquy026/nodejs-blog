d2c9c4434a329c399cc7388fc0d2e4831215954f
lengocquy026 <lengocquy0122@gmail.com>
2022-06-21 20:51:18 +0700

    Day 2

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/home', (req, res) => res.send('Hello, world!'))
-
-app.listen(port, () => console.log(`Listening localhost ${port}`))
